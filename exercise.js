ed0a9ff103b4624f155efb981a164b6032c8ba1d
Aiman Al Eryany <102668388+aiman99aleryany@users.noreply.github.com>
2022-09-17 20:01:06 +0300

    Update exercise.js

--- exercise.js
+++ exercise.js
@@ -1,6 +1,22 @@
+// Use a for loop to print all the numbers from 0 to 10 and their sum. The output should be:
+// 0
+// 1
+// 2
+// 3
+// 4
+// 5
+// 6
+// 7
+// 8
+// 9
+// 10
+// 55
+
+
 let total = 0;
 
 for (let counter = 0; counter <= 10; total += counter++) {
   console.log(counter);
 }
+
 console.log(total);
